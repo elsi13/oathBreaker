5bacac079aa5d45e944ad5a3837329a07b512a2f
mrawe14 <42269673+mrawe14@users.noreply.github.com>
2020-02-14 23:33:13 +0000

    story

---
+++ oathBreaker/Assets/Scripts/StorySwitcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StorySwitcher : MonoBehaviour
+{
+    public Sprite Lu; // I attched these from editor
+    public Sprite Hell;
+
+
+    public int snc;
+    public void changeStory() // make sure to attach this to event trigger
+    {
+
+        switch (snc)
+        {
+
+            case 0:
+                SceneManager.LoadScene("2");
+                snc++; //increase count so it gets higher and switches to different sprite
+                break;
+            case 1:
+                SceneManager.LoadScene("3");
+                snc++;
+                break;
+
+            default:
+                Debug.Log("Error");
+                break;
+        }
+    }
+}
+  
