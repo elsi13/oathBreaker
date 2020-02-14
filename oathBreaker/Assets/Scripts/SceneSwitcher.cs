8ba8bfedb6572733155ddf2a8524286c7ce51669
mrawe14 <42269673+mrawe14@users.noreply.github.com>
2020-02-14 22:01:53 +0000

    commit 1

---
+++ oathBreaker/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Top_scene");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
