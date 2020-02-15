8aa1255c426cdd792a5f1774dc6025fc47898bbd
mcromb <marioncromb@hotmail.co.uk>
2020-02-15 23:06:15 +0000

    if you pet ciwi he jumps
    leap motion compatibility

---
+++ oathBreaker/Assets/Scripts/ciwi.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ciwi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.transform.parent.name.Equals("index"))
+      {
+		GetComponent<Animator>().Play("Jump");
+		GetComponent<Animator>().Play("Jump");
+		//jumps twice
+		//idealy go back to previous scene here
+		GetComponent<Animator>().Play("Idle");
+		
+	  }
+	}
+}
