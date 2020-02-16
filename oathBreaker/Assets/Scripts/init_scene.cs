fbe3d2012b27a8c3a0436f5d3528245f7fd5def9
Lucia Cangarova <luciacangarova13@gmail.com>
2020-02-16 12:16:33 +0000

    Merge with lu5

---
+++ oathBreaker/Assets/Scripts/init_scene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init_scene : MonoBehaviour
+{
+    public static GameObject panel, button, dead_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = GameObject.Find("Canvas/Panel");
+        button = GameObject.Find("Canvas/NextButton");
+        dead_text = GameObject.Find("Canvas/Text (TMP)");
+        panel.gameObject.SetActive(false);
+        dead_text.gameObject.SetActive(false);
+        button.gameObject.SetActive(false);
+    }
+}
